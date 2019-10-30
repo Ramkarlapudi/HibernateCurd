682c76e7b4dd53756e0a1745d29030e717a4e309
Ramkarlapudi <Ramkarlapudi1995@gmail.com>
2019-10-30 22:50:31 +0530

    initial setup

---
+++ src/test/java/com/RamKarlapudi/HibernateCurd/HibernateCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RamKarlapudi.HibernateCurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
